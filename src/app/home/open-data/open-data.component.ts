5eaa673b279a10fe058ffa1b87362c40cab5087b
cengkuru michael <mcengkuru@gmail.com>
2024-07-01 06:15:51 +0300

    Feat: add excel download

--- src/app/home/open-data/open-data.component.ts
+++ src/app/home/open-data/open-data.component.ts
@@ -5,6 +5,8 @@ import { map } from 'rxjs/operators';
 import { CommonModule } from '@angular/common';
 import { ProjectService } from "../../services/project.service";
 import {FlattenedProject} from "../../models/flattened-project.model";
+import * as XLSX from 'xlsx';
+import { saveAs } from 'file-saver';
 
 @Component({
   selector: 'app-open-data',
@@ -47,18 +49,23 @@ export class OpenDataComponent implements OnInit {
         case 'csv':
           dataStr = this.convertToCSV(flattenedProjects);
           fileName += '.csv';
+          this.downloadData(dataStr, fileName);
           break;
         case 'json':
           dataStr = JSON.stringify(flattenedProjects, null, 2);
           fileName += '.json';
+          this.downloadData(dataStr, fileName);
           break;
         case 'txt':
           dataStr = this.convertToTXT(flattenedProjects);
           fileName += '.txt';
+          this.downloadData(dataStr, fileName);
+          break;
+        case 'xlsx':
+          fileName += '.xlsx';
+          this.downloadExcel(flattenedProjects, fileName);
           break;
       }
-
-      this.downloadData(dataStr, fileName);
     } catch (error) {
       console.error('Error downloading data:', error);
       this.error = 'An error occurred while downloading the data. Please try again.';
@@ -66,6 +73,7 @@ export class OpenDataComponent implements OnInit {
       this.downloading = false;
     }
   }
+
 
   private convertToCSV(data: FlattenedProject[]): string {
     if (!data || data.length === 0) return '';
@@ -106,5 +114,47 @@ export class OpenDataComponent implements OnInit {
     a.download = fileName;
     a.click();
     window.URL.revokeObjectURL(url);
+  }
+
+  private downloadExcel(data: FlattenedProject[], fileName: string): void {
+    const worksheet: XLSX.WorkSheet = XLSX.utils.json_to_sheet(data);
+    const workbook: XLSX.WorkBook = { Sheets: { 'data': worksheet }, SheetNames: ['data'] };
+
+    // Styling
+    const headerStyle = {
+      font: { bold: true, color: { rgb: "FFFFFF" } },
+      fill: { fgColor: { rgb: "4472C4" } },
+      alignment: { horizontal: "center" }
+    };
+
+    // Apply styles to header row
+    const range = XLSX.utils.decode_range(worksheet['!ref'] || 'A1');
+    for (let C = range.s.c; C <= range.e.c; ++C) {
+      const address = XLSX.utils.encode_col(C) + "1";
+      if (!worksheet[address]) continue;
+      worksheet[address].s = headerStyle;
+    }
+
+    // Auto-size columns
+    const max_width = 50;
+    const min_width = 10;
+    const colWidths: number[] = [];
+    for (let C = range.s.c; C <= range.e.c; ++C) {
+      let max_length = min_width;
+      for (let R = range.s.r; R <= range.e.r; ++R) {
+        const address = XLSX.utils.encode_cell({c: C, r: R});
+        if (!worksheet[address]) continue;
+        const cell = worksheet[address];
+        const value = cell.v || '';
+        const length = value.toString().length;
+        if (length > max_length) max_length = length;
+      }
+      colWidths[C] = max_length > max_width ? max_width : max_length;
+    }
+    worksheet['!cols'] = colWidths.map(w => ({width: w}));
+
+    const excelBuffer: any = XLSX.write(workbook, { bookType: 'xlsx', type: 'array' });
+    const excelData: Blob = new Blob([excelBuffer], { type: 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet' });
+    saveAs(excelData, fileName);
   }
 }
